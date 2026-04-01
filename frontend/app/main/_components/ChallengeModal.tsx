ce008feb7a342831f8fae95d61de7d82cd6cbff1
Jigyun Kim <jigyun1015@gmail.com>
2026-04-01 11:39:24 +0900

    dynamic score 수정

--- frontend/app/main/_components/ChallengeModal.tsx
+++ frontend/app/main/_components/ChallengeModal.tsx
@@ -95,7 +95,7 @@ export function ChallengeModal({
             Score Type: {selectedChallenge.score_type}
           </div>
           <div className="rounded-lg border border-white/10 bg-white/[0.02] px-3 py-2">
-            POINT: {challenge.dynamic_score ?? challenge.point}
+            POINT: {selectedChallenge.dynamic_score ?? selectedChallenge.point}
           </div>
           <div className="rounded-lg border border-white/10 bg-white/[0.02] px-3 py-2">
             State: {selectedChallenge.state}
